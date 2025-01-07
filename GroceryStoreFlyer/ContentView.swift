10b62341fab8aa493938914dc3215e6ed407a2ff
Marcus Li <mmli@lcs.on.ca>
2025-01-07 11:17:19 -0500

    Initial Commit

---
+++ GroceryStoreFlyer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryStoreFlyer
+//
+//  Created by Marcus Li on 2025-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryStoreFlyer/GroceryStoreFlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFlyerApp.swift
+//  GroceryStoreFlyer
+//
+//  Created by Marcus Li on 2025-01-07.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
